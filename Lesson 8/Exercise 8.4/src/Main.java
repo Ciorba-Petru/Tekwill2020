408ff44945615865416a4abd3d26becbfd1bc692
Ciorba Petru <ciorba.petru1992@gmail.com>
2020-07-16 07:37:06 +0300

    Lesson 8

---
+++ Lesson 8/Exercise 8.4/src/Main.java
@@ -0,0 +1,5 @@
+class Main {
+    public static void main(String[] args) {//change ma1n: "1" with "i"
+        System.out.println("Hello, Java!");
+    }
+}
---
+++ Lesson 8/Exercise 8.5/src/Main.java
@@ -0,0 +1,5 @@
+class Main {
+    public static void main(String[] args) {//return type void and [] for array
+        System.out.println("We are robots!");
+    }
+}
