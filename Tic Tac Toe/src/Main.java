7068f6892e822b8f7c255b9c013400386339396f
Ciorba Petru <ciorba.petru1992@gmail.com>
2020-07-27 15:09:18 +0300

    Primii 2 pasi din joc functioneaza

---
+++ Tic Tac Toe/src/Main.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        TicTacToe game = new TicTacToe();
+
+        game.printBoard();
+    }
+}
+
+
+
+
---
+++ Tic Tac Toe/src/TicTacToe.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class TicTacToe {
+    private char[][] board = new char[3][3];
+    private char currentPlayerMark;
+
+    public void printBoard() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter cells: ");
+        String input = sc.nextLine();
+        int nextChar = 0;
+        System.out.println("---------");
+        for (int i = 0; i < 3; i++) {
+            System.out.print("| ");
+            for (int j = 0; j < 3; j++) {
+                board[i][j] = input.charAt(nextChar++);
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println("|");
+        }
+        System.out.println("---------");
+    }
+}
+
+
+
+
+
