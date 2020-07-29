e1b19d9ae117fd1b35acfbc46824f3ef27dc6509
Ciorba Petru <ciorba.petru1992@gmail.com>
2020-07-29 06:55:39 +0300

    Lesson 10 part II

---
+++ Lesson 10/Exercises 10.7/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        Cats cat1 = new Cats("Luxor", 18);
+        Cats cat2 = new Cats("Vasea", 15);
+        Cats cat3 = new Cats("Misa", 4);
+        Cats cat4 = new Cats("Agnsea", 134);
+        Cats cat5 = new Cats("Else", 23);
+        Cats cat6 = new Cats("afa", 23);
+        Cats cat7 = new Cats("a33fa", 2333);
+
+        System.out.println(Cats.getNumberOfCats());
+    }
+}
