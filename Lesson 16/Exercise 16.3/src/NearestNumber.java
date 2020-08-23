7b156c6e78df42366537ddb82e4d093a4e24cdc8
Ciorba Petru <ciorba.petru1992@gmail.com>
2020-08-24 01:00:25 +0300

    Lesson 16 three exercises

---
+++ Lesson 16/Exercise 16.3/src/NearestNumber.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class NearestNumber {
+    public static void main(String[] args) {
+        ArrayList<Integer> l1 = new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            l1.add(sc.nextInt());
+
+        }
+        System.out.println(l1);
+    }
+}
