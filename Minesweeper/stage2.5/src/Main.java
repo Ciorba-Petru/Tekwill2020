7c4c44c9fd79e95b691af3cadb80013f683d079a
Ciorba Petru <ciorba.petru1992@gmail.com>
2020-08-30 22:58:18 +0300

    deleted

--- Minesweeper/stage2.5/src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Minesweeper mines = new Minesweeper();
-        mines.Minesweeper();
-        mines.printMinesweeper();
-    }
-}
