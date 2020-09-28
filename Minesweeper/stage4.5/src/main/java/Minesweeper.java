9a5c0a3baf59339379258af49931ed62e9e7908e
Ciorba Petru <ciorba.petru1992@gmail.com>
2020-09-28 17:02:22 +0300

    Stage 4

--- Minesweeper/stage4.5/src/main/java/Minesweeper.java
+++ Minesweeper/stage4.5/src/main/java/Minesweeper.java
@@ -1,16 +1,22 @@
-import java.util.Random;
-import java.util.Scanner;
+import java.awt.Point;
+import java.util.*;
 
 public class Minesweeper {
 
     public static void main(String[] args) {
         Minesweeper minesweeper = new Minesweeper();
         minesweeper.randomX();
+        minesweeper.game();
     }
 
+    private static final int NR_OF_FIELDS = 81;
+    private static final char EMPTY_SPACE = '.';
     char[][] minesweeper = new char[9][9];
     Random randNum = new Random();
     Scanner sc = new Scanner(System.in);
+    static List<Point> points = new ArrayList<>();
+    static List<Point> guess = new ArrayList<>();
+    static Map<Point, Character> fields = init();
 
     public Minesweeper() {
         for (int i = 0; i < 9; i++) {
@@ -20,15 +26,47 @@ public class Minesweeper {
         }
     }
 
-    public void changeMinesweeper() {
-        while (true) {
+    boolean finished = false;
 
+    public void game() {
+        while (!finished) {
             System.out.print("Set/delete mines marks (x and y coordinates): ");
             int n = sc.nextInt();
             int m = sc.nextInt();
             int x = n - 1;
             int y = m - 1;
+
+            if (n < 0 || n > 9 || m < 0 || m > 9) {
+                System.out.println("Coordinates should be from 1 to 9!");
+
+            } else if (fields.get(new Point(x, y)) == '.' && (getCharAt(x, y) == "0" || getCharAt(x, y) == "X")) {
+                fields.put(new Point(x, y), '*');
+                guess.add(new Point(x, y));
+                finished = checkIfFinished();
+                printMinesweeper();
+            } else if (fields.get(new Point(x, y)) == '*') {
+                fields.put(new Point(x, y), '.');
+                guess.remove(new Point(x, y));
+                finished = checkIfFinished();
+                printMinesweeper();
+            } else {
+                System.out.println("There is a number here!");
+            }
         }
+        System.out.println("Congratulations! You found all mines!");
+    }
+
+    private static boolean checkIfFinished() {
+        return points.equals(guess);
+    }
+
+    private static Map<Point, Character> init() {
+        Map<Point, Character> fields = new HashMap<>(81);
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++)
+                fields.put(new Point(i, j), '.');
+        }
+        return fields;
     }
 
     public void printMinesweeper() {
@@ -37,7 +75,10 @@ public class Minesweeper {
         for (int i = 0; i < 9; i++) {
             System.out.print(i + 1 + "|");
             for (int j = 0; j < 9; j++) {
-                if (minesweeper[i][j] == 'X') {
+                if (fields.get(new Point(i, j)) == '*') {
+                    System.out.print(minesweeper[i][j] = fields.get(new Point(i, j)));
+                    minesweeper[i][j] = 'X';
+                } else if (minesweeper[i][j] == 'X') {
                     System.out.print('.');
                 } else {
                     System.out.print(getCharAt(i, j));
@@ -86,6 +127,7 @@ public class Minesweeper {
         while (i < numberOfMines) {
             int x = randNum.nextInt(9);
             int y = randNum.nextInt(9);
+            points.add(new Point(x, y));
             if (minesweeper[x][y] == '.') {
                 minesweeper[x][y] = 'X';
                 i++;
@@ -94,3 +136,5 @@ public class Minesweeper {
         printMinesweeper();
     }
 }
+
+
